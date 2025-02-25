5b77b76bbd4307185227720fcd2988fc05a0af9e
Anten Manuuel <anten.manuuel@stonybrook.edu>
2025-02-25 12:15:03 -0500

    update project details and give updated resume pdf

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -36,9 +36,11 @@ const Experience = () => {
       company: "Walgreens",
       period: "June 2024 - August 2024",
       achievements: [
-        "Developed a reconciliation tool with React.js, Tailwind CSS, and Spring Boot, saving $30K in license fees and streamlining Pharma reporting.",
-        "Consolidated data from multiple sources into a single view, boosting clarity and adoption through Figma-driven UI design.",
-        "Executed 10+ micro-service regression tests in Postman, verifying logs in Microsoft Azure to ensure reliable data exchange with major Pharma partners."
+       "Developed a reconciliation reporting tool using React.js, Tailwind CSS, and Spring Boot, replacing Tableau and cutting $30K in licensing costs while streamlining Pharma reporting.",
+       "Created a unified data view by consolidating IC+ and Promote Oracle databases, integrating 10M+ prescriptions and 200M+ drug transactions annually, enabling seamless access and improving reporting clarity.",
+        "Designed user-friendly UI mockups in Figma, increasing adoption by 25% among teams unfamiliar with React, improving clarity and usability.",
+        "Tested Spring Boot microservices, executing 10+ regression tests in Postman and verifying logs in Microsoft Azure, ensuring accurate data exchange for 200M+ prescriptions processed annually."
+
       ]
     },
     {
@@ -48,7 +50,7 @@ const Experience = () => {
       achievements: [
         "Managed 150+ computers, printers, and servers across 7 buildings, sustaining 99% system uptime",
         "Resolved 20+ hardware/software issues for faculty, minimizing downtime and boosting productivity.",
-        "• Installed 25+ computers, modernizing lab spaces and ensuring seamless departmental integration"
+        "Installed 25+ computers, modernizing lab spaces and ensuring seamless departmental integration"
       ]
     }
   ];
@@ -57,3 +59,8 @@ const Experience = () => {
 };
 
 export default Experience;
+
+
+
+
+
